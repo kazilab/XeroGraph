cc4174ded15f8fe03289db6539e7c65df67e7b8b
Kazi Uddin <67221376+kazilab@users.noreply.github.com>
2024-07-20 14:01:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ except FileNotFoundError:
 # Main setup configuration
 setup(
     name='XeroGraph',
-    version='0.0.6',
+    version='0.0.7',
     author='Julhash Kazi',
     author_email='XeroGraph@kazilab.se',
     url='https://www.kazilab.se',
